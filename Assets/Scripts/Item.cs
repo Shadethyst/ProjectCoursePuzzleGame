d40bb142e53748219237c16865a0c6ad75fa81c3
Lassi Kimppa <Lassi.kimppa@gmail.com>
2025-01-30 15:58:13 +0200

    Added sprites to sprite folder

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    public void placeItem(Vector2 pos)
+    {
+        Instantiate(this, pos, Quaternion.identity);
+    }
     // Start is called before the first frame update
     void Start()
     {
