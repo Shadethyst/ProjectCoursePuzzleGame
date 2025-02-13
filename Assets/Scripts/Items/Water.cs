003a33fe9f0c0a3f4ba2a54d239d985c37132b57
Johannes Dahlström <johannes.dahlstrom@gmail.com>
2025-02-13 16:07:04 +0200

    Simplify water flowing code

--- Assets/Scripts/Items/Water.cs
+++ Assets/Scripts/Items/Water.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,10 +7,8 @@ using UnityEngine;
 public class Water : Item
 {
     [SerializeField] protected Item transformElement;
-    private int movementDir;
-    private float movementSpeed;
+    private Vector2 movementDir;
     private Tile nextTile;
-
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +20,7 @@ public class Water : Item
     {
         id = 1;
         range = 1;
-        movementSpeed = 2f;
-        movementDir = 2;
+        
         GameManager.Instance.changeInteractor(1);
         GameManager.Instance.changeMover(1);
         
@@ -49,25 +47,19 @@ public class Water : Item
         {
             Debug.Log("placing item");
             var spawnedItem = Instantiate(this, pos, Quaternion.identity);
-            GridManager.instance.getTileAtPos(pos).addItem(spawnedItem.gameObject.GetComponent<Item>(), id);
-            GridManager.instance.getTileAtPos(pos).checkInteraction();
-            if(GridManager.instance.getTileAtPos(pos).getCoords().y-1 == PlayerController.instance.getOccupiedTile().getCoords().y)
-            {
-                spawnedItem.setMovementDir(0);
-            }
-            else if (GridManager.instance.getTileAtPos(pos).getCoords().y + 1 == PlayerController.instance.getOccupiedTile().getCoords().y)
-            {
-                spawnedItem.setMovementDir(2);
-            }
-            else if (GridManager.instance.getTileAtPos(pos).getCoords().x + 1 == PlayerController.instance.getOccupiedTile().getCoords().x)
-            {
-                spawnedItem.setMovementDir(3);
-            }
-            else if (GridManager.instance.getTileAtPos(pos).getCoords().x - 1 == PlayerController.instance.getOccupiedTile().getCoords().x)
-            {
-                spawnedItem.setMovementDir(1);
-            }
-            Debug.Log("set movement direction: " + movementDir);
+
+            Tile tile = GridManager.instance.getTileAtPos(pos);
+            
+            tile.addItem(spawnedItem.gameObject.GetComponent<Item>(), id);
+            tile.checkInteraction();
+
+            Tile occupied = PlayerController.instance.getOccupiedTile();
+
+            Vector2 delta = tile.getCoords() - occupied.getCoords();
+
+            spawnedItem.setMovementDir(delta);
+
+            Debug.Log("set movement direction: " + delta);
         }
     }
     public override void interact(Item interaction)
@@ -97,63 +89,25 @@ public class Water : Item
     /*
      * chooses new tile to move to based on the Waters movementDir variable
      */
-    private void moveInDir(int direction)
+    private void moveInDir(Vector2 dir)
     {
-        Debug.Log("moving in dir: " + direction);
-        if (direction == 0 && GridManager.instance.getTileAtPos(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 1)) != null)
-        {
-            nextTile = GridManager.instance.getTileAtPos(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 1));
-            Debug.Log("cased direction: " + 0);
-        }
-        else if(direction == 1 && GridManager.instance.getTileAtPos(new Vector2(gameObject.transform.position.x + 1, gameObject.transform.position.y)) != null)
-        {
-            nextTile = GridManager.instance.getTileAtPos(new Vector2(gameObject.transform.position.x + 1, gameObject.transform.position.y));
-            Debug.Log("cased direction: " + 1);
-        }
-        else if(direction == 2 && GridManager.instance.getTileAtPos(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1)) != null)
-        {
-            nextTile = GridManager.instance.getTileAtPos(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1));
-            Debug.Log("cased direction: " + 2);
-        }
-        else if(direction == 3 && GridManager.instance.getTileAtPos(new Vector2(gameObject.transform.position.x - 1, gameObject.transform.position.y)) != null)
-        {
-            nextTile = GridManager.instance.getTileAtPos(new Vector2(gameObject.transform.position.x - 1, gameObject.transform.position.y));
-            Debug.Log("cased direction: " + 3);
-        }
-        /*
-        switch (direction)
-        {
-            case 0:
-                nextTile = GridManager.instance.getTileAtPos(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y+1));
-                Debug.Log("cased direction: " + 0);
-                break;
-            case 1:
-                nextTile = GridManager.instance.getTileAtPos(new Vector2(gameObject.transform.position.x+1, gameObject.transform.position.y));
-                Debug.Log("cased direction: " + 1);
-                break;
-            case 2:
-                nextTile = GridManager.instance.getTileAtPos(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1));
-                Debug.Log("cased direction: " + 2);
-                break;
-            case 3:
-                nextTile = GridManager.instance.getTileAtPos(new Vector2(gameObject.transform.position.x - 1, gameObject.transform.position.y));
-                Debug.Log("cased direction: " + 3);
-                break;
-            default:
-                break;
-        }*/
+        Debug.Log("moving in dir: " + dir);
+
+        nextTile = GridManager.instance.getTileAtPos((Vector2)gameObject.transform.position + dir);
+
         if (nextTile && nextTile.Flowable)
         {
             gameObject.transform.position = nextTile.transform.position;
             GridManager.instance.getTileAtPos(gameObject.transform.position).removeItem(id);
-            GridManager.instance.getTileAtPos(nextTile.transform.position).addItem(gameObject.GetComponent<Item>(), id);
+            nextTile.addItem(gameObject.GetComponent<Item>(), id);
         }
         nextTile = null;
 
     }
-    public void setMovementDir(int dir)
+    public void setMovementDir(Vector2 dir)
     {
-        movementDir = dir;
+        // Mathf.Sign(0) == 1, Math.Sign(0) == 0 :rolleyes:
+        movementDir = new Vector2(Math.Sign(dir.x), Math.Sign(dir.y));
     }
     protected void OnOnGameStateChanged(GameState state)
     {
