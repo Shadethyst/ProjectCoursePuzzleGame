95a1fba70fb38e3cd3c5f5ca9b553b33fe6ba580
Henry Sebastian Friman <hsfrim@utu.fi>
2025-02-26 15:04:25 +0200

    Added sound effect functionalities for Wind and Earth (more coming soon)

---
+++ Assets/Scripts/SoundEffectPlayer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectPlayer : MonoBehaviour
+{
+
+    public AudioSource soundEffect;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        soundEffect = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlaySoundEffect()
+    {
+        if (GameManager.Instance.state == GameState.Story)
+        {
+            soundEffect.mute = true;
+        }
+        else
+        {
+            soundEffect.mute = false;
+            soundEffect.volume = 0.7f;
+            soundEffect.Play();
+        }
+    }
+}
