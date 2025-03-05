ef613f15e71aef438312631f48ea0db0c6f69206
Henry Sebastian Friman <hsfrim@utu.fi>
2025-03-05 04:20:26 +0200

    Created a fire puzzle

---
+++ Assets/PuzzleDoor.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleDoor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject[] firePuzzles;
+    private int puzzlesCompleted;
+    private bool open;
+    void Start()
+    {
+        puzzlesCompleted = 0;
+        open = false;
+        GridManager.instance.getTileAtPos(this.transform.position).setWalkable(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Checking puzzles");
+        foreach(GameObject firePuzzle in firePuzzles)
+        {
+            if (!firePuzzle.GetComponent<FirePuzzle>().GetIsActivated())
+            {
+                puzzlesCompleted = 0;
+            }
+            else
+            {
+                puzzlesCompleted++;
+            }
+        }
+
+        if (puzzlesCompleted >= firePuzzles.Length)
+        {
+            open = true;
+            GridManager.instance.getTileAtPos(this.transform.position).setWalkable(true);
+        }
+
+        if (open)
+        {
+            this.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
